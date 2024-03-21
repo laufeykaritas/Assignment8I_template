d5bbd1d186a4b91b05f8e77f92366b965cd48d1d
laufeykaritas <laufeykaritas@hotmail.com>
2024-03-21 11:06:07 +0100

    Template

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using CollatzFun;
+namespace Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
